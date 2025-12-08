4fe5bc3f83db5102daadd6d874a042480f23704e
David-HxH <david_ems@live.cl>
2025-12-08 04:29:52 -0300

    feat - mejoras UX, se añade btn  modo light o dark.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
 document.addEventListener(`DOMContentLoaded`, () => {
+  const switchTheme = document.getElementById("themeSwitch");
+  const body = document.body;
+
+  // Cargar preferencia guardada
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    body.setAttribute("data-bs-theme", savedTheme);
+    switchTheme.checked = savedTheme === "dark";
+  }
+
+  // Evento de cambio
+  switchTheme.addEventListener("change", () => {
+    const newTheme = switchTheme.checked ? "dark" : "light";
+    body.setAttribute("data-bs-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  });
 
 });
